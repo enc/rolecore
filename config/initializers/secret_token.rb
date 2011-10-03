c86c3ed615f342aef0a9cf516906ba0c4021fb77
Jonatan Reiners <git@encc.de>
2011-10-03 17:04:48 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rolecore::Application.config.secret_token = 'bf009fd01c6279b6d7f028aacf63d8607a659754d668dd2ba96b5ee951ada8a4280f020c40d889d97dcbe720498bafb6be6cc5b103d18a3eb2c7d93cf8fc93f2'
